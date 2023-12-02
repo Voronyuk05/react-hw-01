99ede7c47fd657c5776a0cfaaacd20658cc3ceeb
Ivan <vandoslando@gmail.com>
2023-12-02 10:33:02 +0200

    file v1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+const webSite = {
+  name: 'React',
+  url: 'https://uk.legacy.reactjs.org/docs/create-a-new-react-app.html'
+
+}
+
+const colors = ["Червоний", "Синій", "Зелений"]
+
+let firstNumber = 26
+let secondNumber = 14
+
+const userName = "Іван Воронюк"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <>
+      <h1>{userName}</h1>
+      <a href={webSite.url}>{webSite.name}</a>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img src="https://reactnative.dev/img/logo-og.png" alt="React logo" />
+      <br />
+      <p>{firstNumber + secondNumber}</p>
+      <ul>
+        {colors.map((color) => 
+          <li>{color}</li>
+        )}
+      </ul>
+
+    </>
+  )
 };
